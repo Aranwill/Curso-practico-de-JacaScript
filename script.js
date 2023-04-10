f0a6dcccc32d88cb1a0a310601d81eace6169c3a
Hector Rodriguez <hectorr4747@gmail.com>
2023-04-10 19:09:49 -0300

    add .gitignore

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 
 
-// const h1 = document.querySelector('h1');
-// const p = document.querySelector('p');
-// const parrafito = document.getElementsByClassName('parrafito');
-// const pid = document.getElementById('pid');
-// const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');
 
-// console.log(input.value);
+console.log(input.value);
 
-// console.log({
-//   h1,
-//   p,
-//   parrafito,
-//   pid,
-//   input,
-// });
+console.log({
+  h1,
+  p,
+  parrafito,
+  pid,
+  input,
+});
 
-// h1.innerHTML = 'Hola BEBE <br> Hermoso';
-// h1.innerText = 'Hermoso <br> Bello';
-// // console.log(h1.getAttribute('class'));
-// // h1.setAttribute('class', 'rojo');
+h1.innerHTML = 'Hola BEBE <br> Hermoso';
+h1.innerText = 'Hermoso <br> Bello';
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class', 'rojo');
 
-// h1.classList.add('rojo');
-// h1.classList.remove('verde');
-// // h1.classList.toggle('verde');
-// // h1.classList.contains('verde');
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
 
-// input.value = "456"
+input.value = "456"
 
-// const img = document.createElement('img');
-// img.setAttribute('src', 'https://th.bing.com/th/id/OIP.R1yUuDBkvwa5SnWPFzLlhQHaHa?w=179&h=180&c=7&r=0&o=5&pid=1.7');
-// console.log(img);
+const img = document.createElement('img');
+img.setAttribute('src', 'https://th.bing.com/th/id/OIP.R1yUuDBkvwa5SnWPFzLlhQHaHa?w=179&h=180&c=7&r=0&o=5&pid=1.7');
+console.log(img);
 
-// pid.innerHTML = "";
-// pid.appendChild(img);
+pid.innerHTML = "";
+pid.appendChild(img);
 
 /*-----------Manipulacion del DOM-----------------------*/
 
@@ -64,10 +64,13 @@ function sumarInputValues(event) {
   pResult.innerHTML = "Resultado =  " + sumaInputs;
 };
 
+// cambia el color del parrafo al oprimir el click
 function mouseDown() {
   document.getElementById("color").style.color = "red";
 }
 
+
+// cambia el color del parrafo al soltar el click 
 function mouseUp() {
   document.getElementById("color").style.color = "blue";
 }
